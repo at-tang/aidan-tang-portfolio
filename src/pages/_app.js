76c8aeb63b7787d2d2c0fd18066a635a9e143793
Aidan Tang <aidanroom124@gmail.com>
2024-06-24 19:48:19 -0400

    Added header!

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import Header from "@/components/header";
+import Head from "next/head";
+import Logo from '../images/icons/aidan-tang-icon-bg.jpg'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    
+    <main className="dark:bg-black bg-white">
+
+      <Header/>
+      <Component {...pageProps} />
+
+      <div className="z-50 w-screen h-screen bg-black bg-opacity-50 fixed top-0 left-0 invisible">
+        
+      </div>
+    </main>
+  )
+  ;
 }
